18dbefda2d42b8db937648da4a8a730e20ef09c8
mingwang168 <mingwang0904@gmail.com>
2019-11-20 12:53:51 -0800

    change mammal properties to protected

--- Lab04/Mammal.cs
+++ Lab04/Mammal.cs
@@ -6,24 +6,24 @@ namespace Lab04
 {
     public abstract class Mammal
     {
-        public  string FoodPreference { get; set; }
-        public  string FeedTime { get; set; }
-        public float ServingRatio { get; set; }
-        public string Species { get; set; }
-        public string Type { get; set; }
-        public Mammal(float ServingRatio,string Species)
+        protected string FoodPreference { get; set; }
+        protected string FeedTime { get; set; }
+        protected float ServingRatio { get; set; }
+        protected string Species { get; set; }
+        protected string Type { get; set; }
+        protected Mammal(float ServingRatio,string Species)
         {
             this.Species = Species;
             this.ServingRatio = ServingRatio;
         }
-        public  void PrintDetail(float inputWeight)
+        public void PrintDetail(float inputWeight)
         {
-            Console.WriteLine("Instructions: Keep area secure at all times.");
-            Console.WriteLine($"              Feed at {FeedTime}");
             Console.WriteLine($"Mammal Type:  {Type}");
             Console.WriteLine($"Species:      {Species}");
             Console.WriteLine($"Weight:       {inputWeight}KG");
             Console.WriteLine($"Serving:      {Math.Round(inputWeight * ServingRatio, 3)}KG {FoodPreference}");
+            Console.WriteLine("Instructions: Keep area secure at all times.");
+            Console.WriteLine($"              Feed at {FeedTime}");
         }
     }
 }
